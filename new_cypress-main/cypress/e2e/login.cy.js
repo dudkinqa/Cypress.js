c301c1aafa0852303879670cb1f4e52e1fc83446
Nikita <ndudkin-qa@tutamail.com>
2025-11-10 00:07:39 +0500

    Add files via upload

---
+++ new_cypress-main/cypress/e2e/login.cy.js
@@ -0,0 +1,82 @@
+describe('Проверка авторизации', function () {
+
+   it('Позитивный кейс авторизации: Верный логин и верный пароль', function () {
+        cy.visit('https://login.qa.studio/'); // Зашли на сайт
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // Проверяю цвет кнопки восстановить пароль
+
+        cy.get('#mail').type ('german@dolnikov.ru'); // Ввели верный логин
+        cy.get('#pass').type ('qa_one_love1'); // Ввели верный пароль
+        cy.get('#loginButton').click(); // Нажал войти
+
+        cy.get('#messageHeader').contains('Авторизация прошла успешно'); // Проверяю, что после авторизации вижу текст
+        cy.get('#messageHeader').should('be.visible'); // Текст виден пользователю
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Есть крестик и он виден для пользователя
+    })
+
+    it('Проверка логики восстановления пароля', function () {
+        cy.visit('https://login.qa.studio/'); // Зашли на сайт
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // Проверяю цвет кнопки восстановить пароль
+
+        cy.get('#forgotEmailButton').click(); // Нажал забыли пароль
+        cy.get('#mailForgot').type ('german@dolnikov.ru'); // Ввел почту для восстановления
+        cy.get('#restoreEmailButton').click(); // Нажал отправить код
+
+        cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail'); // Проверяю, что вижу текст
+        cy.get('#messageHeader').should('be.visible'); // Текст виден пользователю
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Есть крестик и он виден для пользователя
+    })
+
+      it('Негативный кейс авторизации: Верный логин и неверный пароль', function () {
+        cy.visit('https://login.qa.studio/'); // Зашли на сайт
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // Проверяю цвет кнопки восстановить пароль
+
+        cy.get('#mail').type ('german@dolnikov.ru'); // Ввели верный логин
+        cy.get('#pass').type ('qa_one_love2'); // Ввели неверный пароль
+        cy.get('#loginButton').click(); // Нажал войти
+
+        cy.get('#messageHeader').contains('Такого логина или пароля нет'); // Проверяю, что после авторизации вижу текст
+        cy.get('#messageHeader').should('be.visible'); // Текст виден пользователю
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Есть крестик и он виден для пользователя
+    })
+
+     it('Негативный кейс авторизации: Неверный логин и верный пароль', function () {
+        cy.visit('https://login.qa.studio/'); // Зашли на сайт
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // Проверяю цвет кнопки восстановить пароль
+
+        cy.get('#mail').type ('german@dolnikov1.ru'); // Ввели неверный логин
+        cy.get('#pass').type ('qa_one_love1'); // Ввели верный пароль
+        cy.get('#loginButton').click(); // Нажал войти
+
+        cy.get('#messageHeader').contains('Такого логина или пароля нет'); // Проверяю, что после авторизации вижу текст
+        cy.get('#messageHeader').should('be.visible'); // Текст виден пользователю
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Есть крестик и он виден для пользователя
+    })
+
+      it('Негативный кейс валидации: Логин без @', function () {
+        cy.visit('https://login.qa.studio/'); // Зашли на сайт
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // Проверяю цвет кнопки восстановить пароль
+
+        cy.get('#mail').type ('germandolnikov.ru'); // Ввели логин без @
+        cy.get('#pass').type ('qa_one_love1'); // Ввели верный пароль
+        cy.get('#loginButton').click(); // Нажал войти
+
+        cy.get('#messageHeader').contains('Нужно исправить проблему валидации'); // Проверяю, что после авторизации вижу текст
+        cy.get('#messageHeader').should('be.visible'); // Текст виден пользователю
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Есть крестик и он виден для пользователя
+    })
+
+     it('Проверка на приведение к строчным буквам в логине', function () {
+        cy.visit('https://login.qa.studio/'); // Зашли на сайт
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // Проверяю цвет кнопки восстановить пароль
+
+        cy.get('#mail').type ('GerMan@Dolnikov.ru'); // Ввели верный логин, но с заглавными буквами
+        cy.get('#pass').type ('qa_one_love1'); // Ввели верный пароль
+        cy.get('#loginButton').click(); // Нажал войти
+
+        cy.get('#messageHeader').contains('Авторизация прошла успешно'); // Проверяю, что после авторизации вижу текст
+        cy.get('#messageHeader').should('be.visible'); // Текст виден пользователю
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Есть крестик и он виден для пользователя
+    })
+})
+
+
